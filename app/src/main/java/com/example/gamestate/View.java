3f83a319c37124486da47a977a61c572764f3ed1
vuh26 <vuh26@up.edu>
2023-03-23 09:16:48 -0700

    Board

--- app/src/main/java/com/example/gamestate/View.java
+++ app/src/main/java/com/example/gamestate/View.java
@@ -3,6 +3,7 @@
 package com.example.gamestate;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.SurfaceView;
 
@@ -11,6 +12,10 @@ public class View extends SurfaceView {
 
     private Model model = new Model();
 
+    int startX;
+    int startY;
+    int endX;
+    int endY;
 
     Paint black = new Paint();
     Paint white = new Paint();
@@ -25,5 +30,36 @@ public class View extends SurfaceView {
         green.setColor(0xFF29AB87);
 
     }
+
+    public void onDraw(Canvas c) {
+        super.onDraw(c);
+        startX = 0;
+        startY = 0;
+        endX = 100;
+        endY = 0;
+
+        c.drawRect(0, 0, 100, 100, green);
+        for (int i = 0; i < 9; i++) {
+            c.drawLine(startX, startY, endX, endY, black);
+            startY += 10;
+            endY += 10;
+        }
+        startX = 0;
+        startY = 0;
+        endX = 0;
+        endY = 100;
+
+        for(int i = 0; i < 9; i++) {
+            c.drawLine(startX, startY, endX, endY, black);
+            startX += 10;
+            endX += 10;
+
+        }
+    }
+
+
+
+
+
 
 }
